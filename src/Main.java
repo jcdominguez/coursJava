06259a7e346bfd2ec13b89998cb4f3d646d80c98
jcdominguez <jcd.pro@gmail.com>
2024-02-20 13:34:45 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,71 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        System.out.println("Hello world!");
+
+
+        char uneLettre = 'b';
+
+        char[] phrase = new char[10];
+        phrase[0] = 'b';
+
+
+        */
+
+        Personne alain; // déclaration variable de type Objet. Par défaut valeur null
+        alain = new Personne();
+        alain.setAge(50);
+        alain.setPrenom("Alain");
+        alain.setNom("Dupont");
+        System.out.println(alain.getNom());
+        alain.feterAnniversaire();
+        System.out.println(alain.getAge());
+
+        Personne marie = new Personne();
+        System.out.println(marie);
+        marie.setPrenom("Marie");
+        marie.setNom("Dufour"); ;
+        marie.setAge(25);
+
+
+        Personne alain2 = alain;
+        alain = marie;
+
+        System.out.println(alain.getPrenom());
+        System.out.println(alain2.getPrenom());
+
+        marie.setPrenom("Maria");
+        System.out.println(marie.getPrenom());
+
+        marie.setAge(-3);
+        System.out.println(marie.getAge());
+
+        String prenom = "Jean";
+        System.out.println(prenom.length());
+
+        // tableau
+        Personne[] annuaire = new Personne[2];
+        annuaire[0] = marie;
+        annuaire[1] = alain;
+
+        // liste : garantie l'ordre et accepte les doublons
+        ArrayList<Personne> personnes = new ArrayList();
+        personnes.add(alain);
+        personnes.add(alain);
+        System.out.println(personnes.size());
+
+        // Set : pas de doublons, ordre non garantie
+        HashSet<Personne> personneSet = new HashSet<>();
+        personneSet.add(alain);
+        personneSet.add(alain);
+        System.out.println(personneSet.size());
+
+
+        // exemple constructeur avec parametres
+        Personne jean = new Personne("Jean", "Dominguez");
+        System.out.println(jean.getPrenom() + " " + jean.getNom());
+    }
+}
---
+++ src/Personne.java
@@ -0,0 +1,45 @@
+public class Personne {
+
+    private int age;
+    private String prenom;
+    private String nom;
+
+
+    public Personne(){
+        System.out.println("constructeur sans parametres");
+    }
+
+    public Personne(String prenom, String nom){
+        this.prenom = prenom;
+        this.nom = nom;
+    }
+
+    public void feterAnniversaire(){
+        age = age + 1;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if(age > 0)
+            this.age = age;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+}
